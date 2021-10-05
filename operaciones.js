a37cca88a7dc476e5060b9da58c0a797ef2212d0
Luis Angel Duran Lopez <luisadl591@gmail.com>
2021-10-05 14:26:25 -0500

    first commit

---
+++ operaciones.js
@@ -0,0 +1,15 @@
+const suma = (a,b) => {
+  return a+b;
+};
+
+const resta = (a,b) => {
+  return a-b;
+};
+
+const multiplicacion = (a,b) => {
+  return a*b;
+}
+
+export {suma, resta, multiplicacion};
+
+export const saludo = 'Hola, soy Luis Angel D, reportandome desde un modulo ';
